b63509fe7755bfc2130d7a1b276e58b8b7dbcc05
unknown <nikolajsok@gmail.com>
2023-05-13 15:01:13 +0200

    camera initial

--- app/src/main/java/si/uni_lj/fri/pbd/gobar/DatabaseHelper.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/gobar/DatabaseHelper.kt
@@ -1,4 +1,24 @@
 package si.uni_lj.fri.pbd.gobar
 
-class DatabaseHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(
+    context, DATABASE_NAME, null, DATABASE_VERSION
+){
+
+    companion object{
+        const val TAG = "DatabaseHelper"
+        const val DATABASE_VERSION = 2
+        const val DATABASE_NAME = "mushroom_db"
+    }
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/si/uni_lj/fri/pbd/gobar/MainActivity.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/gobar/MainActivity.kt
@@ -1,11 +1,109 @@
 package si.uni_lj.fri.pbd.gobar
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
+import si.uni_lj.fri.pbd.gobar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private lateinit var binding : ActivityMainBinding;
+    var dbHelper :DatabaseHelper? = null
+    private var fragmentTransaction : FragmentTransaction? = null
+
+    private var cameraActive :Boolean = true
+
+    /** Helper to ask camera permission.  */
+    object CameraPermissionHelper {
+        private const val CAMERA_PERMISSION_CODE = 0
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+
+        /** Check to see we have the necessary permissions for this app.  */
+        fun hasCameraPermission(activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
+        }
+
+        /** Check to see we have the necessary permissions for this app, and ask for them if we don't.  */
+        fun requestCameraPermission(activity: Activity) {
+            ActivityCompat.requestPermissions(
+                activity, arrayOf(CAMERA_PERMISSION), CAMERA_PERMISSION_CODE)
+        }
+
+        /** Check to see if we need to show the rationale for this permission.  */
+        fun shouldShowRequestPermissionRationale(activity: Activity): Boolean {
+            return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION)
+        }
+
+        /** Launch Application Setting to grant permission.  */
+        fun launchPermissionSettings(activity: Activity) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            intent.data = Uri.fromParts("package", activity.packageName, null)
+            activity.startActivity(intent)
+        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+
+        recreate()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this)
+        }
+
+        dbHelper = DatabaseHelper(this)
+
+        fragmentTransaction = supportFragmentManager.beginTransaction()
+
+
+        var camFragment :CameraFragment = CameraFragment()
+
+
+        fragmentTransaction?.add(R.id.fragmentFrame, camFragment)
+        fragmentTransaction?.commit()
+
+        //todo set different icon
+
+
+        binding.photo.setOnClickListener {
+            if(cameraActive){
+                camFragment.takePhoto()
+            } else {
+                fragmentTransaction = supportFragmentManager.beginTransaction()
+                camFragment = CameraFragment()
+                fragmentTransaction?.add(R.id.fragmentFrame, camFragment)
+                fragmentTransaction?.commit()
+            }
+        }
+    }
+
+
 }
