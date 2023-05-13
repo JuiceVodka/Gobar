bac883326abf2c8c28c27f1c5f4ceb7bb3e0f031
spin311 <svit.spindler@gmail.com>
2023-05-13 13:34:46 +0200

    splash screen

--- app/src/main/java/si/uni_lj/fri/pbd/gobar/SplashScreenActivity.kt
+++ app/src/main/java/si/uni_lj/fri/pbd/gobar/SplashScreenActivity.kt
@@ -1,11 +1,28 @@
 package si.uni_lj.fri.pbd.gobar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val timer: Long = 2000L
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            //replace SplashActivity with MainActivity after timer
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, timer)
+
     }
+
 }
